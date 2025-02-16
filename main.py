f122ba049bacbae113c9aa371d2364c38b946e47
Basil0X7 <116971576+Basil0X7@users.noreply.github.com>
2025-02-16 16:48:41 +0200

    change main.py and README.md

--- main.py
+++ main.py
@@ -8,22 +8,38 @@ app = FastAPI()
 model = joblib.load("models/random_forest_regression.pkl")
 
 class PredictionRequest(BaseModel):
-    z_dr16q: float
-    logmbh_hb: float
-    logmbh_mgii: float
-    logmbh_civ: float
-    logl5100: float
-    logl3000: float
-    logl1350: float
-    hbeta: float
-    mgii: float
-    civ: float
-    logmbh_hb_err: float
-    logmbh_mgii_err: float
-    logmbh_civ_err: float
-    logmbh_err: float
-    gaia_g_flux_snr: float
-    gaia_bp_flux_snr: float
+    Z_DR16Q: float
+    LOGMBH_HB: float
+    LOGMBH_MGII: float
+    LOGMBH_CIV: float
+    LOGMBH: float
+    LOGL5100: float
+    LOGL3000: float
+    LOGL1350: float
+    HBETA_Peak_Wavelength: float
+    HBETA_50_Percent_Flux_Centroid_Wavelength: float
+    HBETA_Flux: float
+    HBETA_Logarithm_of_Line_Luminosity: float
+    HBETA_FWHM: float
+    HBETA_REW: float
+    MGII_Peak_Wavelength: float
+    MGII_50_Percent_Flux_Centroid_Wavelength: float
+    MGII_Flux: float
+    MGII_Logarithm_of_Line_Luminosity: float
+    MGII_FWHM: float
+    MGII_REW: float
+    CIV_Peak_Wavelength: float
+    CIV_50_Percent_Flux_Centroid_Wavelength: float
+    CIV_Flux: float
+    CIV_Logarithm_of_Line_Luminosity: float
+    CIV_FWHM: float
+    CIV_REW: float
+    LOGMBH_HB_ERR: float
+    LOGMBH_MGII_ERR: float
+    LOGMBH_CIV_ERR: float
+    LOGMBH_ERR: float
+    GAIA_G_FLUX_SNR: float
+    GAIA_BP_FLUX_SNR: float
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -88,22 +104,38 @@ def get_ui():
         <div id="container">
             <h1>Black Hole Mass Predictor</h1>
             <form id="predict-form">
-                <input type="number" step="any" name="z_dr16q" placeholder="z_dr16q" required>
-                <input type="number" step="any" name="logmbh_hb" placeholder="logmbh_hb" required>
-                <input type="number" step="any" name="logmbh_mgii" placeholder="logmbh_mgii" required>
-                <input type="number" step="any" name="logmbh_civ" placeholder="logmbh_civ" required>
-                <input type="number" step="any" name="logl5100" placeholder="logl5100" required>
-                <input type="number" step="any" name="logl3000" placeholder="logl3000" required>
-                <input type="number" step="any" name="logl1350" placeholder="logl1350" required>
-                <input type="number" step="any" name="hbeta" placeholder="hbeta" required>
-                <input type="number" step="any" name="mgii" placeholder="mgii" required>
-                <input type="number" step="any" name="civ" placeholder="civ" required>
-                <input type="number" step="any" name="logmbh_hb_err" placeholder="logmbh_hb_err" required>
-                <input type="number" step="any" name="logmbh_mgii_err" placeholder="logmbh_mgii_err" required>
-                <input type="number" step="any" name="logmbh_civ_err" placeholder="logmbh_civ_err" required>
-                <input type="number" step="any" name="logmbh_err" placeholder="logmbh_err" required>
-                <input type="number" step="any" name="gaia_g_flux_snr" placeholder="gaia_g_flux_snr" required>
-                <input type="number" step="any" name="gaia_bp_flux_snr" placeholder="gaia_bp_flux_snr" required>
+                <input type="number" step="any" name="Z_DR16Q" placeholder="Z_DR16Q" required>
+                <input type="number" step="any" name="LOGMBH_HB" placeholder="LOGMBH_HB" required>
+                <input type="number" step="any" name="LOGMBH_MGII" placeholder="LOGMBH_MGII" required>
+                <input type="number" step="any" name="LOGMBH_CIV" placeholder="LOGMBH_CIV" required>
+                <input type="number" step="any" name="LOGMBH" placeholder="LOGMBH" required>
+                <input type="number" step="any" name="LOGL5100" placeholder="LOGL5100" required>
+                <input type="number" step="any" name="LOGL3000" placeholder="LOGL3000" required>
+                <input type="number" step="any" name="LOGL1350" placeholder="LOGL1350" required>
+                <input type="number" step="any" name="HBETA_Peak_Wavelength" placeholder="HBETA_Peak_Wavelength" required>
+                <input type="number" step="any" name="HBETA_50_Percent_Flux_Centroid_Wavelength" placeholder="HBETA_50_Percent_Flux_Centroid_Wavelength" required>
+                <input type="number" step="any" name="HBETA_Flux" placeholder="HBETA_Flux" required>
+                <input type="number" step="any" name="HBETA_Logarithm_of_Line_Luminosity" placeholder="HBETA_Logarithm_of_Line_Luminosity" required>
+                <input type="number" step="any" name="HBETA_FWHM" placeholder="HBETA_FWHM" required>
+                <input type="number" step="any" name="HBETA_REW" placeholder="HBETA_REW" required>
+                <input type="number" step="any" name="MGII_Peak_Wavelength" placeholder="MGII_Peak_Wavelength" required>
+                <input type="number" step="any" name="MGII_50_Percent_Flux_Centroid_Wavelength" placeholder="MGII_50_Percent_Flux_Centroid_Wavelength" required>
+                <input type="number" step="any" name="MGII_Flux" placeholder="MGII_Flux" required>
+                <input type="number" step="any" name="MGII_Logarithm_of_Line_Luminosity" placeholder="MGII_Logarithm_of_Line_Luminosity" required>
+                <input type="number" step="any" name="MGII_FWHM" placeholder="MGII_FWHM" required>
+                <input type="number" step="any" name="MGII_REW" placeholder="MGII_REW" required>
+                <input type="number" step="any" name="CIV_Peak_Wavelength" placeholder="CIV_Peak_Wavelength" required>
+                <input type="number" step="any" name="CIV_50_Percent_Flux_Centroid_Wavelength" placeholder="CIV_50_Percent_Flux_Centroid_Wavelength" required>
+                <input type="number" step="any" name="CIV_Flux" placeholder="CIV_Flux" required>
+                <input type="number" step="any" name="CIV_Logarithm_of_Line_Luminosity" placeholder="CIV_Logarithm_of_Line_Luminosity" required>
+                <input type="number" step="any" name="CIV_FWHM" placeholder="CIV_FWHM" required>
+                <input type="number" step="any" name="CIV_REW" placeholder="CIV_REW" required>
+                <input type="number" step="any" name="LOGMBH_HB_ERR" placeholder="LOGMBH_HB_ERR" required>
+                <input type="number" step="any" name="LOGMBH_MGII_ERR" placeholder="LOGMBH_MGII_ERR" required>
+                <input type="number" step="any" name="LOGMBH_CIV_ERR" placeholder="LOGMBH_CIV_ERR" required>
+                <input type="number" step="any" name="LOGMBH_ERR" placeholder="LOGMBH_ERR" required>
+                <input type="number" step="any" name="GAIA_G_FLUX_SNR" placeholder="GAIA_G_FLUX_SNR" required>
+                <input type="number" step="any" name="GAIA_BP_FLUX_SNR" placeholder="GAIA_BP_FLUX_SNR" required>
                 <button type="submit">Submit</button>
             </form>
             <div id="output"></div>
@@ -135,10 +167,14 @@ def get_ui():
 
 @app.post("/predict")
 def predict(data: PredictionRequest):
-    input_data = np.array([data.z_dr16q, data.logmbh_hb, data.logmbh_mgii, data.logmbh_civ, data.logl5100,
-                           data.logl3000, data.logl1350, data.hbeta, data.mgii, data.civ,
-                           data.logmbh_hb_err, data.logmbh_mgii_err, data.logmbh_civ_err,
-                           data.logmbh_err, data.gaia_g_flux_snr, data.gaia_bp_flux_snr]).reshape(1, -1)
+    input_data = np.array([
+        data.Z_DR16Q, data.LOGMBH_HB, data.LOGMBH_MGII, data.LOGMBH_CIV, data.LOGMBH,
+        data.LOGL5100, data.LOGL3000, data.LOGL1350, data.HBETA_Peak_Wavelength, data.HBETA_50_Percent_Flux_Centroid_Wavelength,
+        data.HBETA_Flux, data.HBETA_Logarithm_of_Line_Luminosity, data.HBETA_FWHM, data.HBETA_REW,
+        data.MGII_Peak_Wavelength, data.MGII_50_Percent_Flux_Centroid_Wavelength, data.MGII_Flux, data.MGII_Logarithm_of_Line_Luminosity,
+        data.MGII_FWHM, data.MGII_REW, data.CIV_Peak_Wavelength, data.CIV_50_Percent_Flux_Centroid_Wavelength, data.CIV_Flux,
+        data.CIV_Logarithm_of_Line_Luminosity, data.CIV_Flux, data.CIV_REW, data.LOGMBH_HB_ERR, data.LOGMBH_MGII_ERR, data.LOGMBH_CIV_ERR,
+        data.LOGMBH_ERR, data.GAIA_G_FLUX_SNR, data.GAIA_BP_FLUX_SNR]).reshape(1, -1)
 
     prediction = model.predict(input_data)
 
